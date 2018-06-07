015db2e506ecdeadbf4e7d44302b2123bea05e84
bastian schnittger <bastian.schnittger@5minds.de>
2018-06-07 11:45:51 +0200

    add first repository contract

---
+++ SolutionExplorerContracts/src/ISolutionExplorerDBRepository.ts
@@ -0,0 +1,9 @@
+import {IIdentity, ISolution, IDiagram} from './index';
+
+export interface ISolutionExplorerDBRepository {
+  openSolution(identity: IIdentity, pathspec: string): boolean;
+  loadSolution(): ISolution;
+  saveSolution(solution: ISolution, path: string): boolean;
+  loadDiagram(diagramName: string): IDiagram;
+  saveDiagram(diagram: IDiagram, path: string): boolean;
+}
