c50ae5b8cbbae626b3d48532f1eab56ef6df75c8
Paul <paul.heidenreich@5minds.de>
2018-06-08 14:57:04 +0200

    :fire: Remove Dist Folder From Index

--- src/SolutionExplorer.Service.Contracts/dist/ISolutionExplorerService.d.ts
+++
@@ -1,8 +0,0 @@
-import { ISolution, IDiagram } from 'solutionexplorer.contracts';
-export interface ISolutionExplorerService {
-    openSolution(pathspec: string): Promise<boolean>;
-    loadSolution(): Promise<ISolution>;
-    saveSolution(solution: ISolution, path: string): Promise<boolean>;
-    loadDiagram(diagramName: string): Promise<IDiagram>;
-    saveDiagram(diagram: IDiagram, path: string): Promise<boolean>;
-}
