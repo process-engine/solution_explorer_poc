899d803743569ec53356927e91282b2e09105aad
bastian schnittger <bastian.schnittger@5minds.de>
2018-06-08 14:03:54 +0200

    add some text cases

--- test/solution.test.js
+++ test/solution.test.js
@@ -1,11 +1,43 @@
 const test = require('../src/skeleton/skeleton');
+const assert = require('assert');
 
 describe('Solution', function() {
-  it('Should load a Solution', () => {
 
+  it('Should open a Solution', async () => {
+    const service = await test;
+    const success = await service.openSolution('http://localhost:8000');
+    assert.ok(success);
+  })
+
+  it('Should load a Solution', async () => {
+    const service = await test;
+    await service.openSolution('http://localhost:8000');
+    const solution = await service.loadSolution();
+    assert.ok(solution);
   });
 
-  it('Should save a Solution', () => {
-
+  it('Should save a Solution', async () => {
+    const service = await test;
+    await service.openSolution('http://localhost:8000');
+    const solution = await service.loadSolution();
+    const success = await service.saveSolution(solution);
+    assert.ok(success)
   });
+
+  it('Should load a diagram', async () => {
+    const service = await test;
+    await service.openSolution('http://localhost:8000');
+    const solution = await service.loadSolution();
+    const diagram = await service.loadDiagram(solution.diagrams[0].name);
+    assert.ok(diagram);
+  });
+
+  it('Should save a daigram', async () => {
+    const service = await test;
+    await service.openSolution('http://localhost:8000');
+    const solution = await service.loadSolution();
+    const diagram = await service.loadDiagram(solution.diagrams[0].name);
+    const success = await service.saveDiagram(diagram);
+    assert.ok(success);
+  })
 })
